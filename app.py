abc235665201af1aa39f31bcc660603c63173629
myname21-meet <myname21@meet.mit.edu>
2019-07-30 03:27:24 -0400

    Flask Routing

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from databases import *
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for 
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "home page" 
+    return  render_template("home.html")
+
+@app.route('/student/<int:student_id>')
+def display_student(student_id):
+    student = query_by_id(student_id)
+    return render_template('student.html', student_id=student_id, student=student)
 
 if __name__ == '__main__':
     app.run(debug=True)
